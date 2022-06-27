5b8ee1451f9d1fd9021f6f2f1995469aa6857a67
“00Amit0” <amit836961@gmail.com>
2022-06-28 02:31:06 +0530

    Adding Table employee_payroll

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,5 +2,15 @@
 
 /*UC1---Creaate Database--------*/
 create Database payroll_service;                          
-use payroll_service;         
+use payroll_service;     
+
+/*UC2---Added table employee_payroll---------*/
+create table employee_payroll
+(
+Id int PRIMARY KEY identity,
+Name varchar(30),
+Salary int,
+StartDate date
+)
+    
           
