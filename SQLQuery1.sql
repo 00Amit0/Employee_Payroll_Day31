426215a8924b3767f29c011d94b6aa9d9dc53622
“00Amit0” <amit836961@gmail.com>
2022-06-28 02:35:06 +0530

    Adding values in Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,4 +13,10 @@ Salary int,
 StartDate date
 )
     
+/*UC3---Inserting values in table------------*/
+insert into employee_payroll(Name,Salary,StartDate) values('Ankit', 1000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Amit', 2000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Tushar', 3000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Milan',4000, GETDATE())
+insert into employee_payroll(Name,Salary,StartDate) values('Harsh', 5000, GETDATE())
           
