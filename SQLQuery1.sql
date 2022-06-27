138e4e48bea61d4c7b27f417b8f9554db5d80e6e
“00Amit0” <amit836961@gmail.com>
2022-06-28 02:26:44 +0530

    Created DataBase Payroll_service

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,6 @@
 --------------WELCOME TO MSSQL QUERIES---------------------
+
+/*UC1---Creaate Database--------*/
+create Database payroll_service;                          
+use payroll_service;         
+          
